1440b27f6b785d3f47c1b9abfee556cee9b8b3c1
archebaldo <zakharov90@gmail.com>
2022-07-22 20:36:06 +0300

    Update main.js - add logic for render ShowMore

--- src/main.js
+++ src/main.js
@@ -11,11 +11,19 @@ import { createFilmDetailsTemplate } from './views/film-details';
 import { generateFilm } from './mocks/generate-film';
 import { generateComment } from './mocks/generate-comment';
 
-const FILMS_COUNT = 20;
+const FILMS_COUNT = 21;
+const FILMS_COUNT_ON_START = 5;
+const FILMS_COUNT_BY_CLICK = 5;
 const COMMENTS_COUNT = 3;
+
+let renderedFilms = FILMS_COUNT_ON_START;
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilm);
 const comments = new Array(COMMENTS_COUNT).fill().map(generateComment);
+
+const alreadyWatchedFilms = films.filter(
+  (film) => film.userDetails.alreadyWatched
+).length;
 
 const render = (container, template, where = `beforeend`) => {
   container.insertAdjacentHTML(where, template);
@@ -25,8 +33,8 @@ const headerElement = document.querySelector(`header`);
 const mainElement = document.querySelector(`main`);
 const footerElement = document.querySelector(`footer`);
 
-render(headerElement, createUserRankTemplate());
-render(mainElement, createNavigationTemplate());
+render(headerElement, createUserRankTemplate(alreadyWatchedFilms));
+render(mainElement, createNavigationTemplate(films));
 render(mainElement, createSortTemplate());
 render(mainElement, createFilmsTemplate());
 
@@ -39,11 +47,34 @@ const filmsContainerElement = filmsElement.querySelector(
   `.films-list__container`
 );
 
-films.forEach((film) =>
-  render(filmsContainerElement, createFilmCardTemplate(film, comments))
-);
+films
+  .slice(0, FILMS_COUNT_ON_START)
+  .forEach((film) =>
+    render(filmsContainerElement, createFilmCardTemplate(film, comments))
+  );
 
-render(filmsListElement, createShowMoreTemplate());
+if (films.length > FILMS_COUNT_ON_START) {
+  render(filmsListElement, createShowMoreTemplate());
+
+  const showMoreButton = filmsListElement.querySelector(
+    `.films-list__show-more`
+  );
+
+  showMoreButton.addEventListener(`click`, () => {
+    films
+      .slice(renderedFilms, renderedFilms + FILMS_COUNT_BY_CLICK)
+      .forEach((film) =>
+        render(filmsContainerElement, createFilmCardTemplate(film, comments))
+      );
+
+    renderedFilms += FILMS_COUNT_BY_CLICK;
+
+    if (renderedFilms >= films.length) {
+      showMoreButton.remove();
+    }
+  });
+}
+
 render(footerElement, createFooterStatisticsTemplate(FILMS_COUNT));
 
 document.body.classList.add(`hide-overflow`);

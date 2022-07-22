3a5a62762134652358051b74f7f95d82ff62e38c
archebaldo <zakharov90@gmail.com>
2022-07-22 20:32:34 +0300

    Update helpers const - add UserRank, Filter

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,1 +1,15 @@
 export const EMOTIONS = [`smile`, `sleeping`, `puke`, `angry`];
+
+export const UserRank = {
+  NOVICE: `novice`,
+  FAN: `fan`,
+  MOVIE_BUFF: `movie buff`,
+};
+
+export const Filter = {
+  Watchlist: (films) =>
+    films.filter((film) => film.userDetails.watchlist).length,
+  History: (films) =>
+    films.filter((film) => film.userDetails.alreadyWatched).length,
+  Favorite: (films) => films.filter((film) => film.userDetails.favorite).length,
+};

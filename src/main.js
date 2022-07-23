1f0cdd431100bab190a8cd467902e4c68982df3c
archebaldo <zakharov90@gmail.com>
2022-07-23 15:47:27 +0300

    Update main.js (replace fn to class)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createUserRankTemplate } from './views/user-rank';
-import { createNavigationTemplate } from './views/navigation';
-import { createSortTemplate } from './views/sort';
-import { createFilmsTemplate } from './views/films';
-import { createFilmsListTemplate } from './views/films-list';
-import { createFilmCardTemplate } from './views/film-card';
-import { createShowMoreTemplate } from './views/show-more';
-import { createFooterStatisticsTemplate } from './views/footer-statistics';
-import { createFilmDetailsTemplate } from './views/film-details';
+import UserRankView from './views/user-rank-view/user-rank-view';
+import NavigationView from './views/navigation-view/navigation-view';
+import SortView from './views/sort-view/sort-view';
+import FilmsView from './views/films-view/films-view';
+import FilmsListView from './views/films-list-view/films-list-view';
+import FilmCardView from './views/film-card-view/film-card-view';
+import ShowMoreButtonView from './views/show-more-button-view/show-more-button-view';
+import FooterStatisticsView from './views/footer-statistics-view/footer-statistics-view';
+import FilmDetailsView from './views/film-details-view/film-details-view';
 
 import { generateFilm } from './mocks/generate-film';
 import { generateComment } from './mocks/generate-comment';
@@ -26,21 +26,21 @@ const alreadyWatchedFilms = films.filter(
 ).length;
 
 const render = (container, template, where = `beforeend`) => {
-  container.insertAdjacentHTML(where, template);
+  container.insertAdjacentElement(where, template);
 };
 
 const headerElement = document.querySelector(`header`);
 const mainElement = document.querySelector(`main`);
 const footerElement = document.querySelector(`footer`);
 
-render(headerElement, createUserRankTemplate(alreadyWatchedFilms));
-render(mainElement, createNavigationTemplate(films));
-render(mainElement, createSortTemplate());
-render(mainElement, createFilmsTemplate());
+render(headerElement, new UserRankView(alreadyWatchedFilms).element);
+render(mainElement, new NavigationView(films).element);
+render(mainElement, new SortView().element);
+render(mainElement, new FilmsView().element);
 
 const filmsElement = mainElement.querySelector(`.films`);
 
-render(filmsElement, createFilmsListTemplate());
+render(filmsElement, new FilmsListView().element);
 
 const filmsListElement = filmsElement.querySelector(`.films-list`);
 const filmsContainerElement = filmsElement.querySelector(
@@ -50,32 +50,31 @@ const filmsContainerElement = filmsElement.querySelector(
 films
   .slice(0, FILMS_COUNT_ON_START)
   .forEach((film) =>
-    render(filmsContainerElement, createFilmCardTemplate(film, comments))
+    render(filmsContainerElement, new FilmCardView(film, comments).element)
   );
 
 if (films.length > FILMS_COUNT_ON_START) {
-  render(filmsListElement, createShowMoreTemplate());
+  const showMoreButtonElement = new ShowMoreButtonView().element;
 
-  const showMoreButton = filmsListElement.querySelector(
-    `.films-list__show-more`
-  );
+  render(filmsListElement, showMoreButtonElement);
 
-  showMoreButton.addEventListener(`click`, () => {
+  showMoreButtonElement.addEventListener(`click`, () => {
     films
       .slice(renderedFilms, renderedFilms + FILMS_COUNT_BY_CLICK)
       .forEach((film) =>
-        render(filmsContainerElement, createFilmCardTemplate(film, comments))
+        render(filmsContainerElement, new FilmCardView(film, comments).element)
       );
 
     renderedFilms += FILMS_COUNT_BY_CLICK;
 
     if (renderedFilms >= films.length) {
-      showMoreButton.remove();
+      showMoreButtonElement.remove();
+      showMoreButtonElement.removeElement();
     }
   });
 }
 
-render(footerElement, createFooterStatisticsTemplate(FILMS_COUNT));
+render(footerElement, new FooterStatisticsView(FILMS_COUNT).element);
 
 document.body.classList.add(`hide-overflow`);
-render(document.body, createFilmDetailsTemplate(films[0], comments));
+render(document.body, new FilmDetailsView(films[0], comments).element);

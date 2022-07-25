2eeb7dcd2c0023153e5e33fdfcb8df30c6988fee
archebaldo <zakharov90@gmail.com>
2022-07-25 19:26:46 +0300

    Update helpers - add fn updateItem

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -39,3 +39,13 @@ export const getUserRank = (filmsCount) => {
     return UserRank.MOVIE_BUFF;
   }
 };
+
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [...items.slice(0, index), update, ...items.slice(index + 1)];
+};

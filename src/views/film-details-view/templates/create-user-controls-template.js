4c9863ed643849b0603ed1d4ea856f095a3319df
archebaldo <zakharov90@gmail.com>
2022-07-27 19:16:27 +0300

    Update VIEWS - FilmDetailsView (add new method - setControlCkickHandler and change markup)

--- src/views/film-details-view/templates/create-user-controls-template.js
+++ src/views/film-details-view/templates/create-user-controls-template.js
@@ -3,19 +3,34 @@ export const createUserControlsTemplate = (userDetails) => {
 
   return `
     <section class="film-details__controls">
-      <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist" ${
-        watchlist ? `checked` : ``
-      }>
+      <input
+        type="checkbox"
+        class="film-details__control-input visually-hidden"
+        id="watchlist"
+        name="watchlist"
+        ${watchlist ? `checked` : ``}
+        data-control-type="watchlist"
+        >
       <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
 
-      <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched" ${
-        alreadyWatched ? `checked` : ``
-      }>
+      <input
+        type="checkbox"
+        class="film-details__control-input visually-hidden"
+        id="watched"
+        name="watched"
+        ${alreadyWatched ? `checked` : ``}
+        data-control-type="alreadyWatched"
+        >
       <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
 
-      <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite" ${
-        favorite ? `checked` : ``
-      }>
+      <input
+        type="checkbox"
+        class="film-details__control-input visually-hidden"
+        id="favorite"
+        name="favorite"
+        ${favorite ? `checked` : ``}
+        data-control-type="favorite"
+        >
       <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
     </section>`;
 };

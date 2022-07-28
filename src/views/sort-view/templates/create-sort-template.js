8d67c26fd1a192266e78bafba607c45170ac4682
archebaldo <zakharov90@gmail.com>
2022-07-28 13:54:42 +0300

    Update VIEWS - SortView (add currentSortType, updateView, restoreListeners and change markup)

--- src/views/sort-view/templates/create-sort-template.js
+++ src/views/sort-view/templates/create-sort-template.js
@@ -1,9 +1,24 @@
-export const createSortTemplate = () => {
+import { SortType } from '../../../helpers/const';
+
+export const createSortTemplate = (currentSortType) => {
   return `
     <ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      ${Object.keys(SortType)
+        .map((key) => {
+          const activeClass =
+            currentSortType === SortType[key].type
+              ? `sort__button--active`
+              : ``;
+          return `
+          <li>
+            <a
+              class="sort__button ${activeClass}"
+              data-sort-type=${SortType[key].type}>
+                ${SortType[key].title}
+            </a>
+          </li>`;
+        })
+        .join(``)}
     </ul>
   `;
 };
